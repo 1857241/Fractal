95ced352f45630f7f9f5e293e329aeca5c65feee
1857241 <emails4eugene@gmail.com>
2023-03-15 15:18:54 -0700

    Complete clover (top right)

--- Fractals v2/src/Driver.java
+++ Fractals v2/src/Driver.java
@@ -15,28 +15,87 @@ public class Driver extends JPanel implements ActionListener {
 
 	public void paint(Graphics g) {
 		super.paintComponent(g);
-		rings(g, 200, 0, 0);
+		//clover(g, 200, 0, 0);
+		//clover2(g, 200, 200, 0);
+		//clover3(g, 200, 0, 200);
+		//clover4(g, 200, 200, 200);
 
+
+		//rings(g, 200, 300, 400);
+		
 
 	}// end of paint method - put code above for anything dealing with drawing -
-	
-	public void squares(Graphics g, int len, int x, int y) {
+	public void rings(Graphics g, int radius, int x, int y) {
+		// each method call draws one part of the fractal
+		g.drawOval(x, y, radius, radius);
+		if(x != 350) {
+			rings(g, radius, x+10, y-10 );
+		}
+		
+		
 		
 	}
 
 	public void clover(Graphics g, int radius, int x, int y) {
-
-	
-
-	}
-
-	public void rings(Graphics g, int radius, int x, int y) {
-		// each method call draws one part of the fractal
+		//top left
 		g.drawOval(x, y, radius, radius);
+		
+		
 		if(radius > 1) {
-			rings(g, radius-10, x+5, y+5);
+			clover(g, radius-10, x+10, y+10);
+			
 		}
+		//top right
+		
+		
 	}
+	public void clover2(Graphics g, int radius, int x, int y) {
+		//top left
+		g.drawOval(x, y, radius, radius);
+		
+		
+		if(radius > 1) {
+			clover2(g, radius-10, x, y+10);
+			
+		}
+		//top right
+		
+		
+	}
+	public void clover3(Graphics g, int radius, int x, int y) {
+		//top left
+		g.drawOval(x, y, radius, radius);
+		
+		
+		if(radius > 1) {
+			clover3(g, radius-10, x+10, y);
+			
+		}
+		//top right
+		
+		
+	}
+	public void clover4(Graphics g, int radius, int x, int y) {
+		//top left
+		g.drawOval(x, y, radius, radius);
+		
+		
+		if(radius > 1) {
+			clover4(g, radius-10, x, y);
+			
+		}
+		//top right
+		
+		
+	}
+	public void triangles(Graphics g, int side) {
+		
+	}
+
+	public void squares(Graphics g, int len, int x, int y) {
+		g.drawRect(x, y, x, y);
+	}
+	
 
 	/**
 	 * Update the positions of the ball and paddle. Update the scores, counter
